9a370dea8fd175493cd7f872313a5a3727c24279
selinaxin87 <111873390+selinaxin87@users.noreply.github.com>
2022-10-30 16:58:28 -0400

    Display average

--- app/unemployment.py
+++ app/unemployment.py
@@ -32,3 +32,18 @@ print("LATEST UNEMPLOYMENT RATE:")
 #print(data[0])
 print(f"{data[0]['value']}%", "as of", data[0]["date"])
 
+# Challenge B
+# 
+# What is the average unemployment rate for all months during this calendar year?
+# ... How many months does this cover?
+
+from statistics import mean
+
+this_year = [d for d in data if "2022-" in d["date"]]
+
+rates_this_year = [float(d["value"]) for d in this_year]
+#print(rates_this_year)
+
+print("-------------------------")
+print("AVG UNEMPLOYMENT THIS YEAR:", f"{mean(rates_this_year)}%")
+print("NO MONTHS:", len(this_year))
