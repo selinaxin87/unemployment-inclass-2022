f0464e6ece35eb2922435e7b12b63672abec153f
selinaxin87 <111873390+selinaxin87@users.noreply.github.com>
2022-10-30 16:51:02 -0400

    Display latest data

--- app/unemployment.py
+++ app/unemployment.py
@@ -14,3 +14,11 @@ response = requests.get(request_url)
 parsed_response = json.loads(response.text)
 print(type(parsed_response))
 pprint(parsed_response)
+
+
+# Challenge A
+#
+# What is the most recent unemployment rate? And the corresponding date? 
+# Display the unemployment rate using a percent sign.
+
+breakpoint()
