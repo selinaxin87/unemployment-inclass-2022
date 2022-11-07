53edb5f31c98803ef1fc2c40f3ae430368c9aac6
selinaxin87 <111873390+selinaxin87@users.noreply.github.com>
2022-11-07 17:17:55 -0500

    refactor price formatting logic

--- app/stocks.py
+++ app/stocks.py
@@ -6,6 +6,9 @@ from pandas import read_csv
 
 from app.alpha import API_KEY
 
+def format_usd(my_price):
+    return f"${my_price:,.2f}"
+
 symbol = input("Please input a crypto symbol (default: 'NFLX'): ") or "NFLX"
 print("SYMBOL:", symbol)
 
@@ -23,13 +26,12 @@ latest = df.iloc[0]
 
 #print(latest["timestamp"])
 #print(latest["close"])
-print("LATEST:", '${:,.2f}'.format(latest["adjusted_close"]), "as of", latest["timestamp"])
+print("LATEST:", format_usd(latest["adjusted_close"]), "as of", latest["timestamp"])
 
 # Challenge B
 #
 # What is the highest high price (formatted as USD)?
 # What is the lowest low price (formatted as USD)?
 
-print("HIGH:", '${:,.2f}'.format(df["high"].max()))
-print("LOW:", '${:,.2f}'.format(df["low"].min()))
-
+print("HIGH:", format_usd(df["high"].max()))
+print("LOW:", format_usd(df["low"].min()))
