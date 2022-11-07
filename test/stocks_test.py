729ee39a39d120e1d2aefc7149562568210d8732
selinaxin87 <111873390+selinaxin87@users.noreply.github.com>
2022-11-07 17:55:17 -0500

    Refactor and tested stocks data

--- test/stocks_test.py
+++ test/stocks_test.py
@@ -1,5 +1,9 @@
 
 from app.stocks import format_usd
+from app.stocks import fetch_stocks_data
+from pandas import DataFrame
+
+
 
 def test_usd_formatting():
 
@@ -8,3 +12,16 @@ def test_usd_formatting():
     assert format_usd(4.5555555) == "$4.56"
 
     assert format_usd(1234567890) == "$1,234,567,890.00"
+
+
+def test_data_fetching():
+
+    result = fetch_stocks_data("NFLX")
+    assert isinstance(result, DataFrame)
+
+    assert "timestamp" in result.columns
+    assert "adjusted_close" in result.columns
+    assert "high" in result.columns
+    assert "low" in result.columns
+
+    assert len(result) >= 100
